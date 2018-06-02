f1139abbf7749d9efb6f219d4408d98d8e051561
trey causey <tcausey808@gmail.com>
2018-06-02 09:35:45 -0400

    initial upload of PW expirelist

---
+++ PWExpireList.ps1
@@ -0,0 +1,6 @@
+﻿#tcausey PW expire script updated 5/31
+Get-ADUser -Filter * -SearchBase "ou=ACAGROUP,dc=advisercompliance,dc=com" -Properties enabled, passwordlastset, passwordneverexpires `
+| Select-Object enabled, passwordneverexpires, passwordlastset, name `
+| Where-Object -Filter { $_.passwordneverexpires -eq $false -AND $_.enabled -eq $true} `
+| Sort-Object passwordlastset | ft name, passwordlastset -AutoSize
+
